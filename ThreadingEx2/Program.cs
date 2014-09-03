f9d9bd9d84eabf1dce6ba77b3f6ea085ea73fe79
JohanHarboe <Johanharboe20@gmail.com>
2014-09-03 10:18:05 +0200

    Threads oprettet

--- ThreadingEx2/Program.cs
+++ ThreadingEx2/Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Threading;
 
 namespace FindSmallest
 {
     class Program
     {
+
+        
+
         //test
         private static readonly int[][] Data = new int[][]{
             new[]{1,5,4,2}, 
             new[]{3,2,4,11,4},
             new[]{33,2,3,-1, 10},
             new[]{3,2,8,9,-1},
-            new[]{1, 22,1,9,-3, 5}
+            new[]{1, 22,1,9,-3, 5}        
+
         };
+
+        private static void threads()
+        {
+            foreach (int[] data in Data)
+            {
+                Thread t1 = new Thread(() => FindSmallest(data));
+                Thread t2 = new Thread(() => FindSmallest(data));
+                Thread t3 = new Thread(() => FindSmallest(data));
+                Thread t4 = new Thread(() => FindSmallest(data));
+                Thread t5 = new Thread(() => FindSmallest(data));
+            }
+        }
 
         private static int FindSmallest(int[] numbers)
         {
@@ -35,8 +52,9 @@ namespace FindSmallest
         {
             foreach (int[] data in Data)
             {
+                Thread t = new Thread(() => FindSmallest(data));
                 int smallest = FindSmallest(data);
-                Console.WriteLine("\t" + String.Join(", ", data) + "\n-> " + smallest);
+                Console.WriteLine("\t" + String.Join(", ", data) + "\n-> " + smallest + t);
             }
         }
     }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -6,9 +6,11 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        
         [TestMethod]
         public void TestMethod1()
         {
+            
         }
     }
 }
